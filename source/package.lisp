f7a337add9c7663bc455ffc7df1674de3b546222
levente.meszaros <levente.meszaros@gmail.com>
2009-09-11 15:25:39 +0000

    Package.

--- source/package.lisp
+++ source/package.lisp
@@ -12,7 +12,13 @@
         :hu.dwim.def
         :hu.dwim.defclass-star
         :hu.dwim.logger
+        :hu.dwim.meta-model
+        :hu.dwim.perec
+        :hu.dwim.rdbms
         :hu.dwim.syntax-sugar
         :hu.dwim.util
         :hu.dwim.wui
-        :iolib))
+        :iolib)
+
+  (:shadowing-import-from :hu.dwim.perec
+                          #:ip-address))
