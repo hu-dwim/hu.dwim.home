0b5d1cfbedcbfb1a833228d8f5fc742ca2a9bc6b
levente.meszaros <levente.meszaros@gmail.com>
2009-09-21 14:47:59 +0000

    Renamed to *.sh.

--- bin/server-loop.sh
+++ bin/server-loop.sh
@@ -19,7 +19,7 @@ echo `date` - normal >>${LOG_DIRECTORY}/start.log
 
 while true; do
 
-  ${PROJECT_HOME}/bin/run-sbcl ${SBCL_HOME} --core ${PROJECT_HOME}/../../${PROJECT_NAME}.core --dynamic-space-size ${DYNAMIC_SPACE_SIZE} --lose-on-corruption --end-runtime-options --no-userinit --no-sysinit --end-toplevel-options >>/var/log/${PROJECT_NAME}/stdout.log 2>&1 $*
+  ${PROJECT_HOME}/bin/run-sbcl.sh ${SBCL_HOME} --core ${PROJECT_HOME}/../../${PROJECT_NAME}.core --dynamic-space-size ${DYNAMIC_SPACE_SIZE} --lose-on-corruption --end-runtime-options --no-userinit --no-sysinit --end-toplevel-options >>/var/log/${PROJECT_NAME}/stdout.log 2>&1 $*
   if [ "$?" -ne "0" ]; then
     echo `date` - exited abnormally, with exit code $? >>${LOG_DIRECTORY}/start.log
     # TODO start it from 1 and make it grow with each restart until an upper bound
