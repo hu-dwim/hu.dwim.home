c83bef48f2d5ffe4bd4ab3d352517a2a6ef6cf11
levente.meszaros <levente.meszaros@gmail.com>
2009-09-24 15:50:10 +0000

    Make them constant.

--- source/server.lisp
+++ source/server.lisp
@@ -35,25 +35,27 @@
 ;;;;;;
 ;;; Production
 
-(def special-variable *http-port-command-line-option*
+(def constant +http-port-command-line-option+
   '("http-port"
     :type integer
     :initial-value #.+default-home-server-port+
     :documentation "The HTTP port where the server will be listening"))
 
-(def function process-http-port-command-line-argument (command-line-arguments)
-  (when-bind http-port (getf command-line-arguments :http-port)
+(def function process-http-port-command-line-argument (arguments)
+  (when-bind http-port (getf arguments :http-port)
     (setf (hu.dwim.wui::port-of (find +default-home-server-port+ (hu.dwim.wui::listen-entries-of *home-server*)
                                       :key #'hu.dwim.wui::port-of))
           http-port)))
 
 (def function executable-toplevel ()
-  (bind ((command-line-options (sort (append (list *help-command-line-option*)
-                                             (list *http-port-command-line-option*)
-                                             *database-command-line-options*
-                                             *generic-command-line-options*)
-                                     #'string< :key #'first))
-         (command-line-arguments (process-command-line-options command-line-options (get-command-line-arguments))))
-    (process-help-command-line-argument command-line-options command-line-arguments)
-    (process-http-port-command-line-argument command-line-arguments)
-    (startup-dwim-server command-line-arguments :hu.dwim.home *home-server* *home-application*)))
+  (bind ((options (sort (append (list +help-command-line-option+)
+                                (list +http-port-command-line-option+)
+                                (list +quiet-command-line-option+)
+                                +database-command-line-options+
+                                +generic-command-line-options+)
+                        #'string< :key #'first))
+         (arguments (process-command-line-options options (get-command-line-arguments))))
+    (process-help-command-line-argument options arguments)
+    (process-http-port-command-line-argument arguments)
+    (process-quiet-command-line-argument arguments)
+    (startup-dwim-server arguments :hu.dwim.home *home-server* *home-application*)))
