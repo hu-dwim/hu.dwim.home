7ada3a977ba3e10457489a067c6e1e9fdcb53563
levente.meszaros <levente.meszaros@gmail.com>
2010-01-25 20:36:16 +0000

    Localization.

--- localization/en.lisp
+++ localization/en.lisp
@@ -6,24 +6,19 @@
 
 (in-package :hu.dwim.wui)
 
-;;;;;;
-;;; Icon
-
 (def localization en
   (class-name.system-test-result "system test result")
   (class-name.test-environment "test environment")
   (class-name.test-result "test result")
-
   (comparison-result.better "better")
   (comparison-result.new "new")
   (comparison-result.same "same")
   (comparison-result.unknown "unknown")
   (comparison-result.worse "worse")
-
   (slot-name.assertion-count "assertion count")
   (slot-name.base-test-result "base test result")
-  (slot-name.comparison-result "comparison result")
   (slot-name.compared-test-result "compared test result")
+  (slot-name.comparison-result "comparison result")
   (slot-name.compile-output "compile output")
   (slot-name.error-count "error count")
   (slot-name.expected-failure-count "expected failure count")
@@ -42,6 +37,5 @@
   (slot-name.test-name "test name")
   (slot-name.test-output "test output")
   (slot-name.test-result "test result")
-
   (test-result.fail "fail")
   (test-result.pass "pass"))
