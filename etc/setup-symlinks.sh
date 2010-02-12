74f26806ed1e47d0d8664d231631b3c40de93ca4
attila.lendvai <attila.lendvai@gmail.com>
2010-02-12 12:04:52 +0000

    no more symlinks for www

--- etc/setup-symlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd `dirname "$0"`/../www/
-
-rm -i wui dojo amCharts
-
-ln -sf ../../hu.dwim.wui/www/wui/ .
-ln -sf ../../hu.dwim.wui/www/dojo/ .
-ln -sf ../../hu.dwim.wui/www/amCharts/ .
