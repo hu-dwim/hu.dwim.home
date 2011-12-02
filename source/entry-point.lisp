5666a578b07711850cbb6efb13e0bdd2bf7ae539
attila.lendvai <attila.lendvai@gmail.com>
2011-12-02 12:04:54 +0000

    follow uri path refactor

--- source/entry-point.lisp
+++ source/entry-point.lisp
@@ -45,7 +45,7 @@
   (cgi-file-broker :path "gitweb/gitweb.cgi"
                    :cgi-file (iolib.pathnames:merge-file-paths "gitweb/gitweb.cgi" *workspace-directory*)
                    :priority 1)
-  (cgi-directory-broker :path-prefix "mailman/"
+  (cgi-directory-broker :path "mailman"
                         :root-directory "/usr/lib/cgi-bin/mailman/"
                         :command-line-transformer (lambda (cgi-file)
                                                     `("sudo" "-n" "-E" "-u" "list" "-g" "www-data" ,(iolib.pathnames:file-path-namestring cgi-file)))
@@ -65,7 +65,7 @@
 ;;; TODO: these entry points should not require session/frame if possible
 ;;; TODO: this repeated code is quite boring
 
-(def entry-point (*home-application* :path-prefix "help/")
+(def entry-point (*home-application* :path "help")
   (with-entry-point-logic (:ensure-session #t :ensure-frame #t)
     (make-frame-root-component-rendering-response :content-component (make-instance 'usage-help/widget))))
 
@@ -80,13 +80,13 @@
              ,@body)
            (make-not-found-response)))))
 
-(def entry-point (*home-application* :path-prefix "project/")
+(def entry-point (*home-application* :path "project")
   (with-entry-point-logic (:ensure-session #t :ensure-frame #t)
     (with-symbol-finding-entry-point-logic (project-name (string-upcase *entry-point-relative-path*) :keyword)
       (awhen (find-project project-name :otherwise #f)
         (make-frame-root-component-rendering-response :content-component (make-value-inspector it))))))
 
-(def entry-point (*home-application* :path-prefix "file/")
+(def entry-point (*home-application* :path "file")
   (with-entry-point-logic (:ensure-session #t :ensure-frame #t)
     (bind ((pathname (merge-pathnames *entry-point-relative-path* *workspace-directory*)))
       (if (and (starts-with-subseq (namestring (truename *workspace-directory*)) (namestring pathname))
@@ -94,24 +94,24 @@
           (make-frame-root-component-rendering-response :content-component (make-value-inspector pathname))
           (make-not-found-response)))))
 
-(def entry-point (*home-application* :path-prefix "definition/")
+(def entry-point (*home-application* :path "definition")
   (with-entry-point-logic (:ensure-session #t :ensure-frame #t)
     (with-symbol-finding-entry-point-logic (name (string-upcase *entry-point-relative-path*))
       (awhen (hu.dwim.presentation::make-definitions name)
         (make-frame-root-component-rendering-response :content-component (make-value-inspector it))))))
 
-(def entry-point (*home-application* :path-prefix "function/")
+(def entry-point (*home-application* :path "function")
   (with-entry-point-logic (:ensure-session #t :ensure-frame #t)
     (with-symbol-finding-entry-point-logic (name (string-upcase *entry-point-relative-path*))
       (awhen (and (typep name 'function-name)
                   (fdefinition name))
         (make-frame-root-component-rendering-response :content-component (make-value-inspector it))))))
 
-(def entry-point (*home-application* :path-prefix "class/")
+(def entry-point (*home-application* :path "class")
   (with-entry-point-logic (:ensure-session #t :ensure-frame #t)
     (with-symbol-finding-entry-point-logic (name (string-upcase *entry-point-relative-path*))
       (awhen (find-class name #f)
         (make-frame-root-component-rendering-response :content-component (make-value-inspector it))))))
 
-(def entry-point (*home-application* :path-prefix "status")
+(def entry-point (*home-application* :path "status")
   (make-server-status-response))
