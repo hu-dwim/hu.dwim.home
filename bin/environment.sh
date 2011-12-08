06dc8b58c2629cec46e38b0448e296f1a8fc2a05
attila.lendvai <attila.lendvai@gmail.com>
2011-12-08 10:51:07 +0000

    we're running with postgres 9.1

--- bin/environment.sh
+++ bin/environment.sh
@@ -12,7 +12,7 @@ export DWIM_MAXIMUM_MEMORY_SIZE="1500Mi"
 export DWIM_DAEMON_USER="home-service"
 export DWIM_TOPLEVEL_FUNCTION="hu.dwim.home::executable-toplevel"
 export DWIM_ASDF_SYSTEM_NAME="hu.dwim.home.all"
-export DWIM_POSTGRESQL_VERSION="8.4"
+export DWIM_POSTGRESQL_VERSION="9.1"
 
 export DWIM_INSTALL_PATH="/opt/${DWIM_PROJECT_NAME}"
 export DWIM_WORKSPACE="${DWIM_INSTALL_PATH}/workspace"
